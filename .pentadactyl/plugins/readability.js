14b8fc9eeef0e370c9f9e7ebe628a0bc9c9a0c33
kikyo <frederick.zou@gmail.com>
2011-06-07 10:35:01 +0800

    added document, use bookmarklet when readability add-on doesn't exists

--- .pentadactyl/plugins/readability.js
+++ .pentadactyl/plugins/readability.js
@@ -1,34 +1,69 @@
-// readability.js -- js
-// @Author:      eric.zou (frederick.zou@gmail.com)
-// @License:     GPL (see http://www.gnu.org/licenses/gpl.txt)
-// @Created:     Sun 05 Jun 2011 07:22:35 PM CST
-// @Last Change: Sun 05 Jun 2011 08:13:09 PM CST
-// @Revision:    17
-// @Description:
-// @Usage:
-// @TODO:
-// @CHANGES:
+"use strict";
+XML.ignoreWhitespace = false;
+XML.prettyPrinting = false;
+var INFO =
+<plugin name="readability" version="0.1.1"
+        href="https://github.com/grassofhust/dotfiles/blob/master/.pentadactyl/plugins/readability.js"
+        summary="Read Comfortably—Anytime, Anywhere"
+        xmlns={NS}>
+    <author email="frederick.zou@gmail.com">Yang Zou</author>
+    <license href="http://opensource.org/licenses/mit-license.php">MIT</license>
+    <project name="Pentadactyl" min-version="1.0"/>
+    <p>
+	Readability is a web &amp; mobile app that zaps online clutter and saves web articles in a comfortable reading view. No matter where you are or what device you use, your reading will be there.
+	</p>
+	<p>
+	This plugin provide one command (<em>:read</em>) and three args ( <em>read/save/send</em>, read is the default arg, send means that Sending to your kindle.) to interact with Readability web services.
+    </p>
+</plugin>;
 
+
+const BOOKMARKLET_READ = "javascript:((function(){window.baseUrl='https://www.readability.com';window.readabilityToken='';var s=document.createElement('script');s.setAttribute('type','text/javascript');s.setAttribute('charset','UTF-8');s.setAttribute('src',baseUrl+'/bookmarklet/read.js');document.documentElement.appendChild(s);})())";
+
+const BOOKMARKLET_SEND = "javascript:((function(){window.baseUrl='https://www.readability.com';window.readabilityToken='';var s=document.createElement('script');s.setAttribute('type','text/javascript');s.setAttribute('charset','UTF-8');s.setAttribute('src',baseUrl+'/bookmarklet/send-to-kindle.js');document.documentElement.appendChild(s);})())";
 
 group.commands.add(["rea[dability]", "rea"],
 	"Read Comfortably—Anytime, Anywhere",
 	function (args) {
-		if (typeof rdb === "undefined") {
-			dactyl.echoerr("Missing readability Add-On", commandline.FORCE_SINGLELINE);
-			dactyl.timeout(function () {dactyl.open('https://www.readability.com/addons', {background:false, where:dactyl.NEW_TAB});}, 1000);
-		}
+		let addon = true;
+		if (typeof rdb === "undefined")
+			addon = false;
+
+		let read = function (addon) {
+			if (addon)
+				rdb.overlay.read();
+			else
+				dactyl.open(BOOKMARKLET_READ, {where:dactyl.CURRENT_TAB});
+		};
+
+		let send = function (addon) {
+			if (addon)
+				rdb.overlay.send_to_kindle();
+			else
+				dactyl.open(BOOKMARKLET_SEND, {where:dactyl.CURRENT_TAB});
+		};
+
+		let save = function (addon) {
+			if (addon)
+				rdb.overlay.save();
+			else {
+				dactyl.echoerr("Missing readability Add-On", commandline.FORCE_SINGLELINE);
+				dactyl.timeout(function () {dactyl.open('https://www.readability.com/addons', {background:false, where:dactyl.NEW_TAB});}, 1000);
+			}
+		};
+
 		if (args.length == 0) {
-			rdb.overlay.read();
+			read();
 		} else {
 			switch (args[0]) {
 				case 'read':
-				rdb.overlay.read();
+				read();
 				break;
 				case 'save':
-				rdb.overlay.save();
+				save();
 				break;
 				case 'send':
-				rdb.overlay.send_to_kindle();
+				send();
 				break;
 
 				default:
