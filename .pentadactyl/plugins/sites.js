1116133718043bf512f2f32ab9fc7473c0ad47e7
kikyo <frederick.zou@gmail.com>
2011-06-22 23:56:28 +0800

    fix for baidu tieba

--- .pentadactyl/plugins/sites.js
+++ .pentadactyl/plugins/sites.js
@@ -2,33 +2,36 @@
 // @Author:      eric.zou (frederick.zou@gmail.com)
 // @License:     GPL (see http://www.gnu.org/licenses/gpl.txt)
 // @Created:     Wed 18 May 2011 12:21:51 PM CST
-// @Last Change: Wed 18 May 2011 01:13:50 PM CST
-// @Revision:    43
+// @Last Change: Wed 22 Jun 2011 11:54:26 PM CST
+// @Revision:    108
 // @Description:
 // @Usage:
 // @TODO:
 // @CHANGES:
 
 // 修复百度贴吧无法中键打开链接，狗日的百度
-// http://www.czcp.co.cc/archives/193
-let FixMiddleClick = {
-	handleEvent: function (e) {
-		if (!e.target || e.target.localName != "a" || e.button != 1
-			|| e.target.href == "") return;
-		e.preventDefault();
-		dactyl.open(e.target.href, {background:true, where:dactyl.NEW_TAB});
-	},
-	init: function () {
-		content.document.removeEventListener("click", FixMiddleClick, false);
-		content.document.addEventListener("click", FixMiddleClick, false);
-
-		content.document.addEventListener("unload", function (e) {
-				content.document.removeEventListener("click", FixMiddleClick, false);
-				content.document.removeEventListener("unload", arguments.callee, false);
+// http://www.czcp.co.cc/archives/229
+let Fixcontenteditable = function (doc) {
+	var richObj = doc.evaluate('//*[@contenteditable]', doc, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+	for ( var i=0 ; i < richObj.snapshotLength; i++ ) {
+		var thisObje = richObj.snapshotItem(i);
+		thisObje.addEventListener("blur", function (e) {
+				e.currentTarget.setAttribute("originalcontenteditable", e.currentTarget.getAttribute("contenteditable"));
+				e.currentTarget.removeAttribute("contenteditable");
 			}, false);
+		["focus", "click"].forEach(function(evt) {
+				thisObje.addEventListener(evt, function (e) {
+						e.currentTarget.setAttribute("contenteditable", e.currentTarget.getAttribute("originalcontenteditable"));
+						e.currentTarget.removeAttribute("originalcontenteditable");
+						e.currentTarget.click();
+					}, false);
+		});
+		thisObje.setAttribute("originalcontenteditable", thisObje.getAttribute("contenteditable"));
+		thisObje.removeAttribute("contenteditable");
 	}
-};
-group.autocmd.add(["LocationChange"], "http://tieba.baidu.com/f?*", function () {
-		FixMiddleClick.init();
-});
 
+}
+group.autocmd.add(["DOMLoad"], "http://tieba.baidu.com/f?*", function () {
+		let doc = arguments[0].doc.valueOf();
+		Fixcontenteditable(doc);
+});
