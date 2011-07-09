4b39348db37da4e6f49230053c76650731cf2a52
kikyo <frederick.zou@gmail.com>
2011-07-09 18:52:42 +0800

    add _mouse_gestures.js fix contextmenu doesn't shown fixes, document update

---
+++ .pentadactyl/plugins/_mouse_gestures.js
@@ -0,0 +1,51 @@
+// _mouse_gestures.js -- js
+// @Author:      eric.zou (frederick.zou@gmail.com)
+// @License:     GPL (see http://www.gnu.org/licenses/gpl.txt)
+// @Created:     Sat 09 Jul 2011 02:10:43 PM CST
+// @Last Change: Sat 09 Jul 2011 06:49:03 PM CST
+// @Revision:    45
+// @Description:
+// @Usage:
+// @TODO:
+// @CHANGES:
+
+// If you can read Chinese, please edit this piece of code
+userContext.MGLIST = [
+	//['UDLR', 'Description', '#id or function or ex commands or key mappings', noremap flag],
+	['L'   , '后退'                     , '#Browser:Back'],
+	['R'   , '前进'                     , '#Browser:Forward'],
+	['RLR' , '关闭当前标签页或者是窗口' , '#cmd_close'],
+	['LD'  , '停止加载当前页'           , '#Browser:Stop'],
+	['LR'  , '撤销关闭标签页'           , '#History:UndoCloseTab'],
+	['U'   , '选择上一个标签页'         , 'gT', true],
+	['D'   , '选择下一个标签页'         , 'gt', true],
+	['DR'  , '关闭当前标签页'           , 'd', true],
+	['LU'  , '到页面最底部'             , function() goDoCommand('cmd_scrollTop')],
+	['LD'  , '到页面最底部'             , function() goDoCommand('cmd_scrollBottom')],
+	['UDR' , '添加当前页到书签'         , ':dialog addbookmark'],
+	['L>R' , '前进'                     , '#Browser:Forward'],
+	['L<R' , '后退'                     , '#Browser:Back'],
+	['W-'  , '选择上一个标签页'         , function() gBrowser.tabContainer.advanceSelectedTab(-1, true)],
+	['W+'  , '选择下一个标签页'         , function() gBrowser.tabContainer.advanceSelectedTab(+1, true)],
+];
+
+/**
+ * code snippets, just copy out them into your configuration
+
+// 最大化和还原窗口
+['', 'Maximize or Restore Window', ':js window.windowState == window.STATE_MAXIMIZED ? window.restore() : window.maximize();'],
+
+// 最小化窗口
+['', 'Minimize Window', ':js window.moveTo(0,0);window.minimize();'],
+
+// 全屏或者取消全屏
+['', 'Toggle Fullscreen', ':em View.Full Screen'],
+
+// 跳转到网站上一级目录
+['', 'Go to Parent Directory', 'gu', true],
+ */
+
+// Please don't modify lines below
+// 请不要修改下面的代码
+if (commands.get("mousegestures"))
+	commands.get("mousegestures").action();
--- .pentadactyl/plugins/mouse_gestures.js
+++ .pentadactyl/plugins/mouse_gestures.js
@@ -2,11 +2,19 @@
 XML.ignoreWhitespace = false;
 XML.prettyPrinting = false;
 
+var MESSAGES = {
+  "en-US": [],
+  "zh-CN": []
+};
+
 var MouseGestures = function() {
 
   const Ci = Components.interfaces;
 
-  if (MouseGestures) MouseGestures.registerEvents('remove');
+  if (window._MouseGestures) {
+    window._MouseGestures.registerEvents('remove');
+    delete window._MouseGestures;
+  }
 
   var Class = function() function() {this.initialize.apply(this, arguments);};
   var MouseGestures = new Class();
@@ -17,10 +25,16 @@ var MouseGestures = function() {
   };
 
   MouseGestures.prototype = {
+    _isMouseDownL: false,
+    _isMouseDownR: false,
+    _isMouseDownM: false,
+    _cancel: false,
+    _gestureType: "鼠标手势",
     initialize: function() {
       this.parseSetting();
 
       var self = this;
+      this.registerEvents('remove');
       this.registerEvents('add');
       window.addEventListener('unload', function() { self.registerEvents('remove'); }, false);
     },
@@ -29,15 +43,15 @@ var MouseGestures = function() {
       for (let [prop, val] in Iterator(obj)) {
         switch ( prop ) {
           case "_enableRocker" :
-            getBrowser().mPanelContainer.removeEventListener("draggesture", this, true);
+            gBrowser.mPanelContainer.removeEventListener("draggesture", this, true);
             if (val)
-              getBrowser().mPanelContainer.addEventListener("draggesture", this, true);
+              gBrowser.mPanelContainer.addEventListener("draggesture", this, true);
             break;
 
           case "_enableWheel" :
-            getBrowser().mPanelContainer.removeEventListener("DOMMouseScroll", this, false);
+            gBrowser.mPanelContainer.removeEventListener("DOMMouseScroll", this, false);
             if (val)
-              getBrowser().mPanelContainer.addEventListener("DOMMouseScroll", this, false);
+              gBrowser.mPanelContainer.addEventListener("DOMMouseScroll", this, false);
             break;
 
           default: 
@@ -53,6 +67,7 @@ var MouseGestures = function() {
       this._showstatus = options["mgshowmsg"];
       this._enableRocker = options["mgrocker"];
       this._enableWheel = options["mgwheel"];
+      this._msgContainer = options["mgmsgcontainer"];
       if (this._enableRocker) this.captureEvents.push('draggesture');
       if (this._enableWheel) this.captureEvents.push('DOMMouseScroll');
       userContext.MGLIST.forEach(function( [gesture, desc, action, noremap] ) {
@@ -62,7 +77,7 @@ var MouseGestures = function() {
           let str = action;
           if (str.charAt(0) == ':') action = function() dactyl.execute(str.substr(1));
           else if (str.charAt(0) == '#') action = function() doCommandByID(str.substr(1));
-          else action = function() modules.events.feedkeys(str, noremap);
+          else action = function() events.feedkeys(str, noremap);
         }
         gestures[gesture] = [desc, action];
       });
@@ -72,15 +87,29 @@ var MouseGestures = function() {
     registerEvents: function(action) {
       var self = this;
       this.captureEvents.forEach(
-        function(type) { getBrowser().mPanelContainer[action + 'EventListener'](type, self, type == 'contextmenu' || type == 'draggesture');
+        function(type) { gBrowser.mPanelContainer[action + 'EventListener'](type, self, type == 'contextmenu' || type == 'draggesture');
       });
     },
     set status(msg) {
-      if (this._showstatus) commandline.echo(msg, null, commandline.FORCE_SINGLELINE);
+      if (this._showstatus) {
+        switch ( this._msgContainer) {
+          case "statusline" :
+            commandline.echo(msg, null, commandline.FORCE_SINGLELINE);
+            break;
+
+          case "indicate":
+            var p = document.getElementById('statusbar-display');
+            p.label = msg;
+
+          default :
+            break;
+        }
+      }
     },
     handleEvent: function(event) {
       switch(event.type) {
       case 'mousedown':
+        this._cancel = false;
         if (event.button == 2) {
           this._isMouseDownR = true;
           this._suppressContext = false;
@@ -89,14 +118,16 @@ var MouseGestures = function() {
             this._isMouseDownR = false;
             this._suppressContext = true;
             this._gesture = 'L>R';
+            this._gestureType = "摇杆手势";
             this.stopGesture(event);
           }
-        } else if (this._enableRocker && event.button == 0) {
+        } else if (event.button == 0) {
           this._isMouseDownL = true;
-          if (this._isMouseDownR) {
+          if (this._isMouseDownR && this._enableRocker) {
             this._isMouseDownL = false;
             this._suppressContext = true;
             this._gesture = 'L<R';
+            this._gestureType = "摇杆手势";
             this.stopGesture(event);
           }
         }
@@ -109,14 +140,20 @@ var MouseGestures = function() {
           this._isMouseDownR = false;
           this._suppressContext = !!this._gesture;
           if (this._enableWheel && this._gesture && this._gesture.charAt(0) == 'W') this._gesture = '';
-          this.stopGesture(event);
-          if (this._shouldFireContext) {      // for Linux & Mac
+          if (!this._cancel) {
+            this._gestureType = "鼠标手势";
+            this.stopGesture(event);
+            this.timerGesture(!this._cancel);
+          }
+          this._cancel = false;
+          if (this._shouldFireContext && !this._cancel) {      // for Linux & Mac
             this._shouldFireContext = false;
             let mEvent = event.originalTarget.ownerDocument.createEvent('MouseEvents');
-            mEvent.initMouseEvent('contextmenu', true, true, aEvent.originalTarget.defaultView, 0, event.screenX, event.screenY, event.clientX, event.clientY, false, false, false, false, 2, null);
+            mEvent.initMouseEvent('contextmenu', true, true, event.originalTarget.defaultView, 0, event.screenX, event.screenY, event.clientX, event.clientY, false, false, false, false, 2, null);
             event.originalTarget.dispatchEvent(mEvent);
           }
-        } else if (this._isMouseDownL && event.button == 0) this._isMouseDownL = false;
+        } else if (this._isMouseDownL && event.button == 0)
+          this._isMouseDownL = false;
         break;
       case 'contextmenu':
         if (this._suppressContext || this._isMouseDownR) {
@@ -132,6 +169,7 @@ var MouseGestures = function() {
           event.stopPropagation();
           this._suppressContext = true;
           this._gesture = 'W' + (event.detail > 0 ? '+' : '-');
+          this._gestureType = "滚轮手势";
           this.stopGesture( event, false );
         }
         break;
@@ -155,7 +193,7 @@ var MouseGestures = function() {
           break;
         }
       }
-      this.timerGesture();
+      this.timerGesture(this._cancel);
       var x = event.screenX, y = event.screenY;
       var distX = Math.abs(x-this._x), distY = Math.abs(y-this._y);
       var threshold = 15/ (gBrowser.selectedBrowser.markupDocumentViewer.fullZoom || 1.0);
@@ -163,38 +201,28 @@ var MouseGestures = function() {
       var dir = distX > distY ? (x<this._x ? 'L' : 'R') : (y<this._y ? 'U': 'D');
       if (dir != this._gesture.slice(-1)) {
         this._gesture += dir;
-        this.status = 'Gesture: ' + this._gesture + (this.GESTURES[this._gesture] ? ' (' + this.GESTURES[this._gesture][0] + ')' : '');
+        this.status = '鼠标手势: ' + this._gesture + (this.GESTURES[this._gesture] ? ' (' + this.GESTURES[this._gesture][0] + ')' : '');
       }
       this._x = x; this._y = y;
     },
     timerGesture: function(isClear) {
       if (this._timer)  clearTimeout(this._timer);
-        this._timer = setTimeout( !isClear ? function(self) self.stopGesture({}, true) : function(self) self._timer = self.status = '', 1500, this);
+        this._timer = setTimeout( !isClear ? function(self) self.stopGesture({}, true) : function(self) self._timer = self.status = '', 1000, this);
     },
     stopGesture: function(event, cancel) {
+      this._cancel = cancel || false;
       if (this._gesture) {
         try {
-          if (cancel) throw 'Gesture Canceled';
+          if (cancel) throw '手势已取消';
 
           let cmd = this.GESTURES[this._gesture] || null;
-/*
-          if ( !cmd && this.GESTURES['*'] ) {
-            for (let key in this.GESTURES['*']) {
-              if (this.GESTURES['*'][key].test(this._gesture)) {
-                cmd = this.GESTURES[key];
-                break;
-              }
-            }
-          }
-*/
-          if (!cmd) throw 'Unknown Gesture: ' + this._gesture;
+          if (!cmd) throw '未知手势: ' + this._gesture;
 
           cmd[1].call(this);
-          this.status = 'Gesture: ' + cmd[0];
+          this.status = this._gestureType+': ' + cmd[0];
         } catch (exp) {
           this.status = exp;
         }
-        this.timerGesture(true);
       }
       this._origDoc = this._links = null;
     }
@@ -248,6 +276,23 @@ options.add(["mgrocker"],
   }
 );
 
+options.add(["mgmsgcontainer"],
+  "",
+  "string",
+  "statusline",
+  {
+    setter: function(value) {
+      if (this.hasChanged && options["mgmsgcontainer"] != value)
+        MG.reloadSetting({_msgContainer: value});
+      return value;
+    },
+    completer: function(context) [
+      ["statusline", "Statusline"],
+      ["indicate", "Message Indicate"]
+    ]
+  }
+);
+
 options.add(["mgwheel"],
   "Enable mouse wheel",
   "boolean",
@@ -261,79 +306,147 @@ options.add(["mgwheel"],
   }
 );
 
-var MG = new MouseGestures();
+group.commands.add(["mouseg[estures]", "mg"],
+  "Reload Mouse Gestures",
+  function (args) {
+    if (args.bang) // 强制重载
+      dactyl.execute(":runtime plugins/_mouse_gestures.js");
 
-var INFO =
-<VimperatorPlugin>
-  <name>Mouse Gestures</name>
-  <name lang='ja'>マウスジェスチャー</name>
-  <description>mouse gestures</description>
-  <description lang='ja'>マウスジェスチャー</description>
-  <version>0.10.1</version>
-  <author>pekepeke</author>
-  <minVersion>2.0pre</minVersion>
-  <maxVersion>2.0pre</maxVersion>
-  <updateURL>https://github.com/vimpr/vimperator-plugins/raw/master/mouse_gestures.js</updateURL>
-  <detail lang='ja'><![CDATA[
-  == .pentadactylrc example ==
-  >||
-  js <<EOM
-  liberator.globalVariables.mousegesture_showmsg = true;  // default is true
-  liberator.globalVariables.mousegesture_rocker = true;  // default is false
-  liberator.globalVariables.mousegesture_wheel = true;  // default is false
-  userContext.MGLIST = [
-    //['UDLR', 'Description', '#id or function or ex commands or key mappings', noremap flag]
-    ['L'  , 'Back', '#Browser:Back'],
-    ['R'  , 'Forward', '#Browser:Forward'],
-    ['RLR', 'Close Tab Or Window', '#cmd_close'],
-    ['LD' , 'Stop Loading Page', '#Browser:Stop'],
-    ['LR' , 'Undo Close Tab', '#History:UndoCloseTab'],
-    ['UL' , 'Select Previous Tab', 'gT', true],
-    ['UR' , 'Select Next Tab', 'gt', true],
-    ['LU' , 'Scroll To Top', function() goDoCommand('cmd_scrollTop')],
-    ['LD' , 'Scroll To Bottom', function() goDoCommand('cmd_scrollBottom')],
-    ['UDR', 'Add Bookmark', ':dialog addbookmark'],
-    ['L>R', 'Forward', '#Browser:Forward'],
-    ['L<R', 'Back', '#Browser:Back'],
-    ['W-' , 'Select Previous Tab', function() gBrowser.tabContainer.advanceSelectedTab(-1, true) ],
-    ['W+' , 'Select Next Tab', function() gBrowser.tabContainer.advanceSelectedTab(+1, true) ],
-  ];
-  EOM
-  ||<
-  == liberator.globalVariables ==
-  - mousegesture_showmsg
-    ジェスチャー情報を表示するかどうか(デフォルト=true:表示する)
-  - mousegesture_rocker
-    ロッカージェスチャを有効にするかどうか(デフォルト=false:無効)
-  - mousegesture_wheel
-    ホイールジェスチャを有効にするかどうか(デフォルト=false:無効)
-  - mousegesture_list
-    ジェスチャー設定。2次元配列で指定してください。
-    [ <UDLR>, <Description>, <Command>, <noremap flag> ]
-    - UDLR
-      ジェスチャーを指定します。
-      UDLRの文字列を指定してください。
-      それぞれ、マウスジェスチャーの↑、↓、←, →に対応しています。
-      一応、ロッカージェスチャー・ホイールジェスチャー等にも暫定で対応しています(別途、オプションを有効にする必要がある)。
-      ロッカージェスチャはL>R(左→右クリック), L<R(右→左クリック)で指定可能。
-      ホイールジェスチャは W-(↓回転), W+(上回転)で指定可能
-    - Description
-      コマンドの説明文。
-    - Command
-      ジェスチャーが実施された際に実行するコマンドを指定します。
-      以下の3通りの指定が可能です。
-        - '#id'
-          document.getElementById(id).doCommand() を実行します。
-        - function() { ... }
-          記述された関数を実行します。
-        - ':[command]'
-          Vimperatorのユーザコマンド [command]を実行します。
-        - '[key]'
-          キーを送ります。
-    - noremap flag
-      キーを送る、かつ、そのキーコードを noremap で処理を行いたい場合、true を指定してください。
-  ]]></detail>
-</VimperatorPlugin>
+    if (args.length == 0)
+      window._MouseGestures.initialize.apply(window._MouseGestures);
+    else {
+      let action = args[0];
+      switch (action) {
+        case "list":
+          list();
+        case "reload":
+        window._MouseGestures.initialize.apply(window._MouseGestures);
+
+        case "toggle": // TODO
+        break;
+
+        default :
+        break;
+      }
+    }
+  },
+  {
+    argCount: "?",
+    bang: true, // TODO
+    completer: function (context, args) {
+      switch (args.completeArg) {
+        case 0:
+        context.completions = [
+          ["list", "List Mouse Gestures Settings"],
+          ["reload", "Reload Settings"],
+          ["toggle", "Toggle Mouse Gestures"]
+        ];
+        break;
+
+        default:
+        break;
+      }
+    }
+  }
+);
+
+function list() {
+  let m = {
+    L: '左',
+    R: '右',
+    D: '下',
+    U: '上'
+  };
+  let pattern = /^L<R|W\+|W-|L>R|[LRDU]+$/;
+  let rows = <></>;
+  userContext.MGLIST.forEach(function (item) {
+    let human = "";
+    if (pattern.test(item[0])) {
+        switch (item[0]) {
+        case "L<R":
+        human = "右->左";
+        break;
+
+        case "L>R":
+        human = "左->右";
+        break;
+
+        case "W-":
+        human = "滚轮向后";
+        break;
+
+        case "W+":
+        human = "滚轮向前";
+        break;
+
+        default :
+        Array.slice(item[0]).forEach(function (char) {
+            human += m[char];
+        });
+        break;
+
+      }
+    } else {
+      human = "手势不明";
+    }
+    let noremap = item[3] || false;
+    if (typeof item[2] != "string")
+      noremap = "N/A";
+    else {
+      if (/^[#:]/.test(item[2]))
+        noremap = "N/A";
+    }
+    let row = <><tr>
+      <td>{item[0]}</td>
+      <td>{human}</td>
+      <td>{item[1]}</td>
+      <td>{item[2]}</td>
+      <td>{noremap}</td>
+      </tr></>;
+    rows += row;
+  });
+  let table = <><table style="width:800px;text-algin:left;">
+    <caption>当前可用手势</caption>
+    <thead><tr><th>手势</th><th>人类可读</th><th>描述</th><th>行为</th><th>内置</th></tr></thead>
+    <tbody>{rows}</tbody>
+    <tfoot>
+      <tr><th colspan="5">手势说明</th></tr>
+      <tr><td colspan="5">
+      <table style="width:100%;"><tbody><td style="width:24%;">U:  上</td><td style="width:24%;">D:  下</td><td style="width:24%;">L:  左</td><td style="width:24%;">R:  右</td></tbody></table>
+      </td></tr>
+      <tr><td colspan="5">
+        <table style="width:100%;"><tbody>
+          <tr>
+            <td style="width:49%;"><dl><dt><![CDATA[L<R: 摇杆手势]]></dt>
+              <dd>按住鼠标右键,同时点击鼠标左键</dd>
+            </dl></td>
+            <td><dl><dt><![CDATA[L>R: 摇杆手势]]></dt>
+              <dd>按住鼠标左键,同时点击鼠标右键</dd>
+            </dl></td>
+          </tr>
+          <tr>
+            <td style="width:49%;"><dl><dt>W-: 滚轮手势</dt>
+              <dd>按住鼠标右键,同时向前拨动滚轮</dd>
+            </dl></td>
+            <td><dl><dt>W+: 滚轮手势</dt>
+              <dd>按住鼠标右键,同时向后拨动滚轮</dd>
+            </dl></td>
+          </tr>
+        </tbody></table>
+      </td></tr>
+    </tfoot>
+    </table></>;
+    table += <><style type="text/css"><![CDATA[
+        table {line-height:24px;}
+        caption,th {text-align:left;}
+        th,dt {text-weight:bolder;}
+        tfoot {padding-top:10px;}
+    ]]></style></>;
+  dactyl.echo(table);
+}
+
+var MG = new MouseGestures();
+window._MouseGestures = MG;
 
 var INFO =
 <plugin name="mouse_gestures.js" version="0.10.1"
@@ -353,5 +466,8 @@ var INFO =
 
 /**
  * 另外它还支持摇杆手势和滚轮手势，可以设置超时取消手势，能在 Pentadactyl 状态栏显示当前正在使用的手势（会自动消失）。缺陷是不能显示鼠标轨迹，不支持 FireGestuers 的弹出菜单以及'打开鼠标划过的所有链接'功能.
- * remove EventListener :rehash
+ * 添加更多预设的自定义手势
+ * 不知道脚本,能不能识别左上,左下这样子的手势?这个做为最大化,最小化最合适了.感觉
+ * 其中 L>R 没有问题，但是 L<R 会连续关闭掉两个左侧的标签页，似乎被连续执行了两次。
+ * 脚本加载时检查是否所有的手势都合法,是否有重复定义项
  */
