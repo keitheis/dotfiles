1ebdc31a29b142a41624a72c8cb579996dad6c3a
kikyo <frederick.zou@gmail.com>
2011-10-03 17:30:51 +0800

    fixes - shorten.js

--- .pentadactyl/plugins/shorten.js
+++ .pentadactyl/plugins/shorten.js
@@ -1,46 +1,53 @@
 var shortenURL = function (args) {
-	let url = args.join(" ") || buffer.URL;
+	let url = args[0] || buffer.URL;
 	if (url == "")
 		dactyl.echomsg("Please specifically a uri.", commandline.FORCE_SINGLELINE);
-	else
-		url = encodeURIComponent(url);
 	let invert = options.get("shorten-yank").value;
 	if (args.bang)
 		invert = !invert;
 	// let pattern = /(http:\\)?((is.gd\/[a-zA-Z0-9]{5})|)/gi; // the candy
-	var req = new XMLHttpRequest();
 	var callback = function (data) {
 		if (invert)
-			dactyl.clipboardWrite(data.trim(), true, 'selection');
+			dactyl.clipboardWrite(data.trim(), true, options["shorten-clipboard"]);
 		else
 			dactyl.echomsg(data.trim(), commandline.FORCE_SINGLELINE);
 	};
 	let engine = options.get("shorten-engine").value;
-	if (args["-e"])
-		engine = args["-e"];
+	// TODO: unique
+	if (args["-b"])
+		engine = "b";
+	if (args["-g"])
+		engine = "g";
+	if (args["-i"])
+		engine = "i";
 	switch (engine) {
 		case 'b' :
-			bitly(req, url, callback);
+			bitly(url, callback);
 			break;
 		case 'g' :
-			google(req, url, callback);
+			google(url, callback);
 			break;
 		case 'i' :
-			isgd(req, url, callback);
+			isgd(url, callback);
 			break;
 		default :
 			break;
 	}
 };
 
-var bitly = function (req, url, callback) {
+var bitly = function (url, callback) {
 	/**
 	 * Reference
 	 * http://code.google.com/p/bitly-api/
 	 */
+	if (options["shorten-user-bitly"].length == 0)
+		return dactyl.echoerr("Bitly username doesn't exists!");
+	if (options["shorten-key-bitly"].length == 0)
+		return dactyl.echoerr("Your bitly api key doesn't exists!.");
 	if (!(/^https?:\/\//.test(url)))
-		url = "http://" + url;
-	var querystring = "login=grassofhust&apiKey=R_696fd603075a1cedeb0fd56bbaa58033&format=txt&longUrl="+url;
+		url = encodeURIComponent("http://" + url); // TODO: check
+	var req = new XMLHttpRequest();
+	var querystring = "login="+options["shorten-user-bitly"]+"&apiKey="+options["shorten-key-bitly"]+"&format=txt&longUrl="+url;
 	req.open("GET",
 		"http://api.bit.ly/v3/shorten?" + querystring,
 		true
@@ -57,7 +64,7 @@ var bitly = function (req, url, callback) {
 	req.send(null);
 };
 
-var google = function (req, url, callback) {
+var google = function (url, callback) {
 	/**
 	 * Reference
 	 * Google URL Shortener
@@ -67,6 +74,7 @@ var google = function (req, url, callback) {
 	 * Google URL Shortener API
 	 * http://code.google.com/apis/urlshortener/index.html
 	 */
+	var req = new XMLHttpRequest();
 	req.open("POST",
 		"https://www.googleapis.com/urlshortener/v1/url",
 		true
@@ -74,8 +82,9 @@ var google = function (req, url, callback) {
 	req.setRequestHeader('Content-Type', 'application/json');
 	var data = {
 		"longUrl" : url,
-		"key" : "AIzaSyDaUYAFnNQB3X4__ZuwcPfc8nGNwhItDPk"
 	};
+	if (options["shorten-key-google"].length)
+		data['key'] = options["shorten-key-google"];
 	req.send(JSON.stringify(data));
 	req.onreadystatechange = function (ev) {
 		if (req.readyState == 4) {
@@ -89,13 +98,14 @@ var google = function (req, url, callback) {
 	};
 };
 
-var isgd = function (req, url, callback) {
+var isgd = function (url, callback) {
 	/**
 	 * Reference
 	 * API
 	 * http://is.gd/apishorteningreference.php
 	 */
-	var querystring = "format=simple&url="+url;
+	var req = new XMLHttpRequest();
+	var querystring = "format=simple&url="+encodeURIComponent(url);
 	req.open("GET",
 		"http://is.gd/create.php?" + querystring,
 		true
@@ -110,7 +120,7 @@ var isgd = function (req, url, callback) {
 		}
 	};
 	req.send(null);
-}
+};
 
 let opt = [];
 [
@@ -126,17 +136,6 @@ let opt = [];
 	});
 });
 
-opt.push({
-	names: ["-e"],
-	description: "URL Shorten Engine",
-	type: CommandOption.STRING,
-	completer: [
-		["b","Bitly helps you share, track, and analyze your links."],
-		["g", "Google URL Shortener"],
-		["i", "is.gd - a URL shortener. Mmmm, tasty URLs!"]
-	]
-});
-
 // Add custom commands
 group.commands.add(["shor[ten]", "sht"],
 	"Shorten current url or specific url",
@@ -144,6 +143,7 @@ group.commands.add(["shor[ten]", "sht"],
 	{
 		argCount: "*",
 		bang: true,
+		literal: 0,
 		options: opt
 	},
 	true
@@ -168,3 +168,35 @@ group.options.add(["shorten-yank", "shty"],
 	true
 );
 
+group.options.add(["shorten-key-google", "shkg"],
+	"Requests to the Google URL Shortener API for public data must be accompanied by an identifier, which can be an API key or an auth token.",
+	"string",
+	""
+);
+
+group.options.add(["shorten-user-bitly", "shub"],
+	"Bitly username.",
+	"string",
+	""
+);
+
+group.options.add(["shorten-key-bitly", "shkb"],
+	"Your bitly api key.",
+	"string",
+	""
+);
+
+group.options.add(["shorten-clipboard", "shcb"],
+	"ClipBoard.",
+	"string",
+	"",
+	{
+		completer: function (context) [
+			["global", "global clipboard"],
+			["selection", "selection clipboard"]
+		],
+		validator: function (value) {
+			return ["", "global", "selection"].indexOf(value) >= 0;
+		}
+	}
+);
