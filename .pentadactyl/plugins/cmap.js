892d62c9100fa0e95f3bd5ece9aebe81782baf5b
kikyo <frederick.zou@gmail.com>
2012-02-13 00:28:42 +0800

    support <A-Return> -- cmap.js

--- .pentadactyl/plugins/cmap.js
+++ .pentadactyl/plugins/cmap.js
@@ -2,24 +2,30 @@
 // @Author:      eric.zou (frederick.zou@gmail.com)
 // @License:     GPL (see http://www.gnu.org/licenses/gpl.txt)
 // @Created:     Mon 27 Jun 2011 09:59:53 PM CST
-// @Last Change: Sun 12 Feb 2012 08:00:59 AM CST
-// @Revision:    118
+// @Last Change: Sun 12 Feb 2012 09:59:34 PM CST
+// @Revision:    143
 // @Description:
 // @Usage:
 // @TODO:
 // @CHANGES:
 // 在 caret 模式下输入命令之后，光标会丢失？
-
-group.mappings.add([modes.EX], ["<S-Return>"], "Toggle !, or specific feature for open/tabopen/winopen", function () {
-        let command = commandline.command;
-        commands.repeat = command; // TODO: more work
-        return function () {
-            modes.pop();
-            execute(command);
-        };
+[
+	[[modes.EX], ["S"], "Toggle !, or specific feature for open/tabopen/winopen"],
+	[[modes.EX], ["C"], "Force new tab."],
+	[[modes.EX], ["A"], "Force background tab."]
+].forEach(function (args) {
+	let [mode, keys, description] = args;
+	group.mappings.add(mode, keys.map(function(key) '<'+key+'-Return>'), description, function () {
+		let command = commandline.command;
+		commands.repeat = command; // TODO: more work
+		return function () {
+			modes.pop();
+			execute(command, keys);
+		};
+	});
 });
 
-function execute(str, modifiers, silent) {
+function execute(str, keys, modifiers, silent) {
     // skip comments and blank lines
     if (/^\s*("|$)/.test(str))
         return;
@@ -33,70 +39,57 @@ function execute(str, modifiers, silent) {
         if (command === null)
             throw FailedAssertion(_("dactyl.notCommand", config.appName, args.commandString));
 
-        switch (command.name) {
-            case "open" :
-            case "tabopen" :
-            case "winopen" :
-            if (args[0]) {
-                var urls = args[0].split(/\s+\|\s+/).map(function (str) {
-                        return str.replace(/^(?:www\.)?(.+?)(?:\.com)?$/g , "$1.com");
-                });
-                var seps = args[0].match(/\s+\|\s+/g) || [];
-                seps.push("");
-                var serializes = [];
-                for (let [idx, elem] in Iterator(urls)) {
-                    serializes.push(elem);
-                    serializes.push(seps[idx]);
-                }
-                args[0] = serializes.join("");
-            }
-            break;
+		if (keys.toSource() == ["S"].toSource()){
+			switch (command.name) {
+				case "open" :
+				case "tabopen" :
+				case "winopen" :
+				if (args[0]) {
+					var urls = args[0].split(/\s+\|\s+/).map(function (str) {
+							return str.replace(/^(?:www\.)?(.+?)(?:\.com)?$/g , "$1.com");
+					});
+					var seps = args[0].match(/\s+\|\s+/g) || [];
+					seps.push("");
+					var serializes = [];
+					for (let [idx, elem] in Iterator(urls)) {
+						serializes.push(elem);
+						serializes.push(seps[idx]);
+					}
+					args[0] = serializes.join("");
+				}
+				break;
 
-            default :
-            // if command support bang option
-            if (command.bang)
-                args.bang = !args.bang;
-            break;
-        }
-        res = res && command.execute(args, modifiers);
-    }
-    return res;
-}
+				default :
+				// if command has bang option
+				if (command.bang)
+					args.bang = !args.bang;
+				break;
+			}
+		} else if (keys.toSource() == ["C"].toSource()) {
+			switch (command.name) {
+				case "tab" :
+				break;
 
-group.mappings.add([modes.EX], ["<C-Return>"], "Force new tab.", function () {
-        let command = commandline.command;
-        commands.repeat = command;
-        return function () {
-            modes.pop();
-            tab(command);
-        };
-});
+				default :
+				var params = [command.name];
+				params = params.concat(Array.slice(args));
+				args[0] = params.join(" ");
+				command = commands.get("tab");
+				break;
+			}
+		} else if (keys.toSource() == ["A"].toSource()) {
+			switch (command.name) {
+				case "background" :
+				break;
 
-function tab(str, modifiers, silent) {
-    // skip comments and blank lines
-    if (/^\s*("|$)/.test(str))
-        return;
-
-    modifiers = modifiers || {};
-
-    if (!silent)
-        commands.lastCommand = str.replace(/^\s*:\s*/, "");
-    let res = true;
-    for (let [command, args] in commands.parseCommands(str.replace(/^'(.*)'$/, "$1"))) {
-        if (command === null)
-            throw FailedAssertion(_("dactyl.notCommand", config.appName, args.commandString));
-
-        switch (command.name) {
-            case "tab" :
-            break;
-
-            default :
-            var params = [command.name];
-            params = params.concat(Array.slice(args));
-            args[0] = params.join(" ");
-            command = commands.get("tab");
-            break;
-        }
+				default :
+				var params = [command.name];
+				params = ["tab"].concat(params).concat(Array.slice(args));
+				args[0] = params.join(" ");
+				command = commands.get("background");
+				break;
+			}
+		}
         res = res && command.execute(args, modifiers);
     }
     return res;
